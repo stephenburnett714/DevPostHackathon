d7d369fd999586effbda91ff6b5f38c91aa8d3b5
Stephen Burnett <sburnett1990@gmail.com>
2023-10-06 19:46:48 -0400

    added backend and front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-blue-300'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="text-blue-200">
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
