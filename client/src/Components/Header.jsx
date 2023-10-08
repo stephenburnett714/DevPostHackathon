c23d592ca7bc8caa351bcf4fb161b1b3192cbdee
Quan Nguyen <jacob.nguyenaq@gmail.com>
2023-10-08 12:12:22 -0700

    commit

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import '../CSS/header.css';
 import { useNavigate } from 'react-router';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Header() {
- const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
 
   function loginNavigate() {
     navigate('/login');
   }
+
+  function signUpNavigate() {
+    navigate('/signup');
+  }
+
+  function homeNavigate() {
+    navigate('/');
+  }
+
+  function eventNavigate() {
+    navigate('/events');
+  }
+
+  const keySearch = (event) => {
+    if (event.key === 'Enter') {
+      eventNavigate();
+    }
+  };
+
+  const isSpecificPage = () => {
+    return location.pathname === '/profile';
+  };
+
   return (
     <header className="w-full flex flex-col sm:flex-row justify-between items-center px-2 sm:px-10 py-6 bg-white-800 shadow-md">
       {/* Left Content */}
@@ -16,6 +41,7 @@ export default function Header() {
           src="/src/assets/img/Logo_block.png"
           alt="Logo"
           className="w-13 sm:w-32 mr-4 cursor-pointer"
+          onClick={homeNavigate}
         />
 
         <form action="">
@@ -30,23 +56,43 @@ export default function Header() {
               placeholder="Search events"
               className="inter-font pl-10 py-1 border rounded-md focus:outline-none focus:border-gray-500 flex-shrink-0 w-full sm:max-w-md"
               style={{ maxWidth: '350px', width: '100%' }}
+              onKeyDown={keySearch}
             />
           </div>
         </form>
       </div>
       {/* Right Content */}
-      <div className="flex items-center">
-        <ul className="flex items-center space-x-5">
-          <li>
-            <a onClick={loginNavigate} className="inter-font ww-purple font-normal">Login</a>
-          </li>
-          <li>
-            <button className="inter-font ww-purple-bg text-white px-4 py-2 rounded-md">
-              Sign up
-            </button>
-          </li>
-        </ul>
-      </div>
+      {isSpecificPage() ? (
+        <div className="flex shrink gap-8 pt-4 sm:pt-0 cursor-pointer">
+          <img src="/src/assets/img/message_icon.svg" />
+          <img src="/src/assets/img/noti_icon.svg" />
+          <div className="flex gap-1">
+            <img src="/src/assets/img/Frame 51.png" className="w-10 h-10" />
+            <img src="/src/assets/img/drop_box.svg" />
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center">
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a
+                onClick={loginNavigate}
+                className="inter-font ww-purple font-normal cursor-pointer"
+              >
+                Login
+              </a>
+            </li>
+            <li>
+              <button
+                className="inter-font ww-purple-bg text-white px-4 py-2 rounded-md"
+                onClick={signUpNavigate}
+              >
+                Sign up
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
